a8668a32fb352e7af8f9a91b3614fe7afb8f30de
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-08-31 14:20:40 +0500

    Update Question 01 (Part d)(Basic Maths).asm

--- Lab Tasks/Lab 02/Question 01 (Part d)(Basic Maths).asm
+++ Lab Tasks/Lab 02/Question 01 (Part d)(Basic Maths).asm
@@ -1,6 +1,6 @@
 INCLUDE Irvine32.inc        ;Mutahir Ahmed Khan 24k-0030 
 .data  
-	msg db "Total is: ", 0
+
 .code
 main PROC   
 
@@ -11,9 +11,6 @@ main PROC
 	add eax, 1011110b
 	sub eax, 0Dh
 	add eax, 12
-
-	mov edx, OFFSET msg
-	call WriteString 
 
 	call WriteInt
 
