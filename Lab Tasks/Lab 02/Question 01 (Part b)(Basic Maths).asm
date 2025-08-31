29f40554af797cd4dbe008885f1220c738900c07
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-08-31 14:19:44 +0500

    Update Question 01 (Part b)(Basic Maths).asm

--- Lab Tasks/Lab 02/Question 01 (Part b)(Basic Maths).asm
+++ Lab Tasks/Lab 02/Question 01 (Part b)(Basic Maths).asm
@@ -1,17 +1,12 @@
 INCLUDE Irvine32.inc
 .data  
 
-    msg db "Final Balance: ", 0 ;From Manual
-
 .code
 main PROC
     mov eax, 95
     sub eax,31
     add eax,240
     sub eax, 125
-
-    mov edx, OFFSET msg ;From Manual
-    call WriteString
 
     call WriteInt
                       
