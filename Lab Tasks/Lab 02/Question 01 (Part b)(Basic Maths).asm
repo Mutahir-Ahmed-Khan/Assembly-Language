929faef70e63e1f7f5c21e8dc414013b051129f4
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-08-31 14:05:29 +0500

    Create Question 01 (Part b)(Basic Maths).asm

---
+++ Lab Tasks/Lab 02/Question 01 (Part b)(Basic Maths).asm
@@ -0,0 +1,21 @@
+INCLUDE Irvine32.inc
+.data  
+
+    msg db "Final Balance: ", 0 ;From Manual
+
+.code
+main PROC
+    mov eax, 95
+    sub eax,31
+    add eax,240
+    sub eax, 125
+
+    mov edx, OFFSET msg ;From Manual
+    call WriteString
+
+    call WriteDec
+                      
+
+exit
+main ENDP
+END main
