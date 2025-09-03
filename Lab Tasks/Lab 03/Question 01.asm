c3ac21eac3863dfacb4f6d79da2cc36ff3aedcff
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-09-03 11:52:07 +0500

    Update Question 01.asm

--- Lab Tasks/Lab 03/Question 01.asm
+++ Lab Tasks/Lab 03/Question 01.asm
@@ -1,8 +1,8 @@
 INCLUDE Irvine32.inc ;Mutahir Ahmed Khan 24k-0030 
 .data
-	snum SBYTE, -100
-	unum BYTE, 200 
-	holder BYTE, ?
+	snum SBYTE -100
+	unum BYTE 200 
+	holder BYTE ?
 .code
 main PROC
 
