f8054ee2571586b1acf417d06dedc9c0dacf5ff8
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-09-13 01:21:25 +0500

    Create Task 08.asm

---
+++ Lab Tasks/Lab 04 (Home-Task)/Task 08.asm
@@ -0,0 +1,14 @@
+INCLUDE Irvine32.inc ;Mutahir Ahmed Khan 24k-0030
+;comment are for own understanding
+
+.data
+
+	sales SBYTE 12,32,34,53,24,19,83
+
+.code
+main PROC
+
+
+exit
+main ENDP
+END main
