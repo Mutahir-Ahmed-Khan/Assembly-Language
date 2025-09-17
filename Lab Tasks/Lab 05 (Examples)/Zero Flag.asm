4b2b4b5c042011b92dad273b6be8c4f2c869e866
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-09-17 20:35:05 +0500

    Update Zero Flag.asm

--- Lab Tasks/Lab 05 (Examples)/Zero Flag.asm
+++ Lab Tasks/Lab 05 (Examples)/Zero Flag.asm
@@ -1,1 +1,13 @@
-
+INCLUDE Irvine32.inc        ;Mutahir Ahmed Khan 24k-0030 
+;Comments for own understanding
+.data 
+.code
+main PROC 
+	
+	mov ax, 0FFFFh 
+	inc ax 
+	call DumpRegs
+	
+exit
+main ENDP
+END main
