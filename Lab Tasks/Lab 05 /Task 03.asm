20f93ae6f03a1821ad0c77f377d028a73aec9095
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-09-26 18:31:22 +0500

    Create Task 03.asm

---
+++ Lab Tasks/Lab 05 /Task 03.asm
@@ -0,0 +1,15 @@
+INCLUDE Irvine32.inc        ;Mutahir Ahmed Khan 24k-0030 
+;Comments for own understanding
+.data 
+	
+	array1 WORD 45,32,71,44,92
+	array2 WORD 32,44,45,71,92
+
+.code
+main PROC 
+
+
+
+exit
+main ENDP
+END main
