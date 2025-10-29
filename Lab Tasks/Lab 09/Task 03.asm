f67aae9590b235c0c55c606c6f2ae1d6b9c93702
Mutahir Ahmed Khan <178812198+Mutahir-Ahmed-Khan@users.noreply.github.com>
2025-10-29 12:39:00 +0500

    Update Task 03.asm

--- Lab Tasks/Lab 09/Task 03.asm
+++ Lab Tasks/Lab 09/Task 03.asm
@@ -1,6 +1,9 @@
 INCLUDE Irvine32.inc
 .data
 
+;24k-0030
+;Mutahir Ahmed Khan
+
 	enc BYTE ?
 	msg BYTE "Enter	Binary: ", 0
 	fiMsg BYTE "Encrypted: ", 0
@@ -8,12 +11,13 @@ INCLUDE Irvine32.inc
 .code
 main PROC
 
-		call ReadInt
+		mov al, 10110110b
 		rol eax, 2
 		shr eax, 1
 
 		mov edx, OFFSET fiMsg
 		call WriteString
+		movzx eax, al
 		call WriteBin
 	
 
